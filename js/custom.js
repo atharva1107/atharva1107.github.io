2298433b3bcaaf34215fe704cdcafc5538331dc0
Atharva Raibagi <atharvaraibagi@gmail.com>
2018-11-13 16:07:31 -0600

    Trial paragraph animation

--- js/custom.js
+++ js/custom.js
@@ -169,7 +169,8 @@ var Messenger = function(el){
   var m = this;
   
   m.init = function(){
-    m.codeletters = "&#*+%?£@§$";
+  	
+    m.codeletters = "&#*+%?£@§$10";
     m.message = 0;
     m.current_length = 0;
     m.fadeBuffer = false;
@@ -257,5 +258,32 @@ var Messenger = function(el){
 console.clear();
 var messenger = new Messenger($('#messenger'));
 
+//Para animation
+// Wrap every letter in a span
+$('.ml12').each(function(){
+  $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+});
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml12 .letter',
+    translateX: [40,0],
+    translateZ: 0,
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 1200,
+    delay: function(el, i) {
+      return 500 + 30 * i;
+    }
+  }).add({
+    targets: '.ml12 .letter',
+    translateX: [0,-30],
+    opacity: [1,0],
+    easing: "easeInExpo",
+    duration: 1100,
+    delay: function(el, i) {
+      return 100 + 30 * i;
+    }
+  });
 
 }(jQuery));
