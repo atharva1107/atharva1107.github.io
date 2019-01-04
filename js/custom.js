3d124627aaaf124610d121e230c669c240d2e533
Atharva Raibagi <atharvaraibagi@gmail.com>
2019-01-04 11:14:00 -0600

    Add new header image

--- js/custom.js
+++ js/custom.js
@@ -161,7 +161,22 @@ $(window).load(function(){
 
 // Skill set
 
+// Progress Bar
+//  var pageProgress = function() {
+//    $(window).scroll(function() {
+ //     var wintop = $(window).scrollTop(), docheight = $('.page').height(), winheight = $(window).height();
+      // console.log(wintop);
+  //    var totalScroll = (wintop/(docheight-winheight))*100;
+      // console.log("total scroll" + totalScroll);
+ //     $(".KW_progressBar").css("width",totalScroll+"%");
+   // });
 
+ // };
+// pageProgress();
+
+
+//});
+//
  
 // Animation
 var Messenger = function(el){
